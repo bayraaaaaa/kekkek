fbbb3a274cebdba8bf0599b88055ecc9f32d45c4
Dulguuuuuuuun <Dulguunpalam23@gmail.com>
2023-04-15 12:16:44 +0800

    hashpassword hiisn

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv"
 import cors from "cors"
 import userRouter from "./router/user.js";
+import authorizationRouter from "./router/authorization.js";
 
 const app = express()
 dotenv.config();
@@ -14,6 +15,7 @@ const uri = process.env.MONGODB || "";
  app.use(cors()); 
  app.use(express.json());
  app.use("/user", userRouter)
+ app.use("/authorization",authorizationRouter)
 
 
 const connect = () => {
--- backend/model/User.js
+++ backend/model/User.js
@@ -1,5 +1,5 @@
 import mongoose, {Schema} from "mongoose";
-
+import bcrypt from "bcrypt"
 const UserSchema = new mongoose.Schema({
     username:{type:String },
     email:{type:String},
@@ -10,7 +10,16 @@ const UserSchema = new mongoose.Schema({
 
 })
 
-
+UserSchema.pre("save",async function(nest){
+    try {
+        const salt = await bcrypt.genSalt(10) 
+        const hashPassword = await bcrypt.hash(this.password,salt)
+        this.password = hashPassword
+        nest()
+    } catch (error) {
+        nest(error)
+    }
+})
 
 const User = mongoose.model("User", UserSchema);
 
