2cd3b6cda959b56c3bbc93eab68e04a532010a53
HOP2e-Zolboot <hop2e-zolboot@Fulxe-4.local>
2023-04-20 16:39:41 +0800

    instructorpage brg dussn

--- frontend/mergejil/src/App.jsx
+++ frontend/mergejil/src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Navbar from "./components/navbar";
-import Container from "react-bootstrap/Container";
+import Instructor from "./pages/InstracturPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <div style={{ backgroundColor: "gray", width: "100%", height: "100vh" }}>
-        <Navbar />
-      </div>
+      {/* <div style={{ backgroundColor: "gray", width: "100%", height: "100vh" }}>
+        {/* <Navbar/> */}
+      {/* </div> */}
+      <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Navbar />}></Route>
+        <Route path="/Instructor" element={<Instructor />}></Route>
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
