f8673c83244dd84cd6fcb4966c3edc90d3ede3d8
bayr_aaaaa <bayra_aaaaa@bayr-aaaaas-MacBook-Pro.local>
2023-04-27 17:05:01 +0800

    halit ehlsn recentPost

--- frontend/mergejil/src/App.jsx
+++ frontend/mergejil/src/App.jsx
@@ -6,33 +6,30 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Overlay from "./components/overlay";
 import Section from "./components/section";
-
+import RecentPost from "./components/recentPost";
 import Testimonial from "./components/testimonial";
-
 
 function App() {
   return (
     <>
+      <RecentPost />
       {/* <div style={{ backgroundColor: "gray", width: "100%", height: "100vh" }}>
 
         {/* <Navbar/> */}
       {/* </div> */}
-      <BrowserRouter>
+      {/* <BrowserRouter>
       <Routes>
       <Route path="/" element={<Navbar />}></Route>
         <Route path="/Instructor" element={<Instructor />}></Route>
       </Routes>
     </BrowserRouter>
 
-        <Navbar />
-      </div> */}
+        <Navbar /> */}
+      {/* </div> */}
       {/* <Overlay /> */}
       {/* <Section /> */}
-      <Testimonial />
-
-      
-      <Section />
-
+      {/* <Testimonial /> */}
+      {/* <Section /> */}
     </>
   );
 }
