7d8f8826a92db6e17d326fb757e6d6eb10097554
Dulguuuuuuuun <Dulguunpalam23@gmail.com>
2023-04-29 13:32:01 +0800

    infoSidebar hiisen

--- frontend/mergejil/src/App.jsx
+++ frontend/mergejil/src/App.jsx
@@ -1,27 +1,18 @@
-
 import React from "react";
 import Navbar from "./components/navbar";
 import Instructor from "./pages/InstracturPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-
-
-
-
+import InformationSidebar from "./components/InformationSidebar";
 function App() {
   return (
     <>
-
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Navbar />}></Route>
           <Route path="/Instructor" element={<Instructor />}></Route>
-          <Route path="/footer" element={<Footer />}></Route>
+          <Route path="/footer" element={<InformationSidebar />}></Route>
         </Routes>
       </BrowserRouter>
-
-
-
     </>
   );
 }
